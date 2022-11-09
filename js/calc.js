89b6d1d3f478a10669241a26049bace77f6103a8
Timur <radjabovtima@mail.ru>
2022-11-09 14:25:26 +0500

    end

--- js/calc.js
+++ js/calc.js
@@ -1,97 +1,149 @@
 document.addEventListener("DOMContentLoaded", () => {
   const slider01 = document.querySelector(".slider01");
   const slider02 = document.querySelector(".slider02");
-  const progress = document.querySelector(".point01");
-  const progress02 = document.querySelector(".point02");
+  const sliderOne = document.querySelector(".slider-one");
+  const sliderTwo = document.querySelector(".slider-two");
+  const progressOne = document.querySelector(".point01");
+  const progress02One = document.querySelector(".point02");
+  const progressTwo = document.querySelector(".point03");
+  const progress02Two = document.querySelector(".point04");
   const screenWidth = window.screen.width;
-  const result01 = document.querySelector(".result");
+  const resultOne = document.querySelector(".result");
+  const resultTwo = document.querySelector(".result-two");
 
-  function customSlider() {
-    if (slider02.value === "0") {
+  function customSlider(one, two, progress, progress02, result01) {
+    console.log(progress);
+    if (two.value === "0") {
       const profit = new Intl.NumberFormat("ru-RU").format(
-        slider01.value * 1 * 321 * 0.075
+        Math.round(one.value * 1 * 321 * 0.075)
       );
       result01.innerHTML = profit;
     } else {
       const profit = new Intl.NumberFormat("ru-RU").format(
-        slider01.value * slider02.value * 321 * 0.075
+        Math.round(one.value * two.value * 321 * 0.075)
       );
       result01.innerHTML = profit;
     }
 
-    if (slider01.value === "2000") {
-      progress.style.width = slider01.value - 2000 + "px";
+    if (one.value === "2000") {
+      progress.style.width = one.value - 2000 + "px";
     }
-    if (slider01.value === "2100") {
-      progress.style.width = slider01.value - 1993 + "px";
+    if (one.value === "2100") {
+      progress.style.width = one.value - 1990 + "px";
     }
-    if (slider01.value === "2200") {
-      progress.style.width = slider01.value - 1990 + "px";
+    if (one.value === "2200") {
+      progress.style.width = one.value - 1990 + "px";
     }
-    if (slider01.value === "2300") {
-      progress.style.width = slider01.value - 1988 + "px";
+    if (one.value === "2300") {
+      progress.style.width = one.value - 1988 + "px";
     }
-    if (slider01.value === "2400") {
-      progress.style.width = slider01.value - 1987 + "px";
+    if (one.value === "2400") {
+      progress.style.width = one.value - 1987 + "px";
     }
-    if (slider01.value === "2500") {
-      progress.style.width = slider01.value - 1986 + "px";
+    if (one.value === "2500") {
+      progress.style.width = one.value - 1986 + "px";
     }
-    if (slider01.value === "2600") {
-      progress.style.width = slider01.value - 1986 + "px";
+    if (one.value === "2600") {
+      progress.style.width = one.value - 1986 + "px";
     }
-    if (slider01.value === "2700") {
-      progress.style.width = slider01.value - 1988 + "px";
+    if (one.value === "2700") {
+      progress.style.width = one.value - 1988 + "px";
     }
-    if (slider01.value === "2800") {
-      progress.style.width = slider01.value - 1987 + "px";
+    if (one.value === "2800") {
+      progress.style.width = one.value - 1987 + "px";
     }
-    if (slider01.value === "2900") {
-      progress.style.width = slider01.value - 1987 + "px";
+    if (one.value === "2900") {
+      progress.style.width = one.value - 1987 + "px";
     }
-    if (slider01.value === "3000") {
-      progress.style.width = slider01.value - 1987 + "px";
+    if (one.value === "3000") {
+      progress.style.width = one.value - 1987 + "px";
     }
 
-    if (slider02.value === "0") {
-      progress02.style.width = slider02.value + 0 + "px";
+
+    if (one.value === "1750") {
+      progress.style.width = one.value - 1750 + "px";
     }
-    if (slider02.value === "3") {
-      progress02.style.width = 128 - slider02.value + "px";
+    if (one.value === "1850") {
+      console.log(one.value);
+      progress.style.width = one.value - 1741 + "px";
     }
-    if (slider02.value === "6") {
-      progress02.style.width = 243 - slider02.value + "px";
+    if (one.value === "1950") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "9") {
-      progress02.style.width = 359 - slider02.value + "px";
+    if (one.value === "2050") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "12") {
-      progress02.style.width = 474 - slider02.value + "px";
+    if (one.value === "2150") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "15") {
-      progress02.style.width = 585 - slider02.value + "px";
+    if (one.value === "2250") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "18") {
-      progress02.style.width = 698 - slider02.value + "px";
+    if (one.value === "2350") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "21") {
-      progress02.style.width = 815 - slider02.value + "px";
+    if (one.value === "2450") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "24") {
-      progress02.style.width = 928 - slider02.value + "px";
+    if (one.value === "2550") {
+      progress.style.width = one.value - 1740 + "px";
     }
-    if (slider02.value === "27") {
-      progress02.style.width = 1037 - slider02.value + "px";
+    if (one.value === "2650") {
+      progress.style.width = one.value - 1740 + "px";
+    }
+    if (one.value === "2750") {
+      progress.style.width = one.value - 1740 + "px";
+    }
+
+    if (two.value === "0") {
+      progress02.style.width = two.value + 0 + "px";
+    }
+    if (two.value === "3") {
+      progress02.style.width = 128 - two.value + "px";
+    }
+    if (two.value === "6") {
+      progress02.style.width = 243 - two.value + "px";
+    }
+    if (two.value === "9") {
+      progress02.style.width = 359 - two.value + "px";
+    }
+    if (two.value === "12") {
+      progress02.style.width = 474 - two.value + "px";
+    }
+    if (two.value === "15") {
+      progress02.style.width = 585 - two.value + "px";
+    }
+    if (two.value === "18") {
+      progress02.style.width = 698 - two.value + "px";
+    }
+    if (two.value === "21") {
+      progress02.style.width = 815 - two.value + "px";
+    }
+    if (two.value === "24") {
+      progress02.style.width = 928 - two.value + "px";
+    }
+    if (two.value === "27") {
+      progress02.style.width = 1037 - two.value + "px";
     }
   }
 
-  customSlider();
+  customSlider(slider01, slider02, progressOne, progress02One, resultOne);
+  customSlider(sliderOne, sliderTwo, progressTwo, progress02Two, resultTwo);
 
   slider01.addEventListener("input", () => {
-    customSlider();
+
+    customSlider(slider01, slider02, progressOne, progress02One, resultOne);
   });
 
   slider02.addEventListener("input", () => {
-    customSlider();
+
+    customSlider(slider01, slider02, progressOne, progress02One, resultOne);
+  });
+  sliderOne.addEventListener("input", () => {
+    customSlider(sliderOne, sliderTwo, progressTwo, progress02Two, resultTwo);
+  });
+
+  sliderTwo.addEventListener("input", () => {
+    customSlider(sliderOne, sliderTwo, progressTwo, progress02Two, resultTwo);
   });
 });
